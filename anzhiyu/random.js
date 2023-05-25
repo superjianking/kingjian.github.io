ccc9661ad15e0459386aa12dc35d1853f65ddab7
superjianking <2548092637@qq.com>
2023-05-25 17:24:16 +0800

    Site updated: 2023-05-25 17:24:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/13/Linux驱动基础知识一/","2023/05/24/一段Python代码破解WiFi/","2023/05/18/初识WEB前端(初识HTML) 二/","2023/05/19/初识WEB前端(HTML表单) 三/","2023/05/18/初识WEB前端(基础知识) 一/","2023/05/18/面试题/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/13/Linux驱动基础知识一/","2023/05/24/一段Python代码破解WiFi/","2023/05/19/初识WEB前端(HTML表单) 三/","2023/05/18/初识WEB前端(初识HTML) 二/","2023/05/18/初识WEB前端(基础知识) 一/","2023/05/18/面试题/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
